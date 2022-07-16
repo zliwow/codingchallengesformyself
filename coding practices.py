bb6589b778ea06ecbd5d29c5a6b08ba52dee0a22
Andrew Zihao Li <zliwow123@gmail.com>
2022-07-15 17:45:59 -0700

    modified:   coding practices.py

--- coding practices.py
+++ coding practices.py
@@ -30,4 +30,21 @@ def generate(numRows):
 
 
 
-# generate(5) 0 1 0 top of the tree
+# generate(5) 
+# 0 1 0 top of the tree
+
+# LC 119 E Pascal's Triangle II
+def getRow(rowIndex):
+        res = [[1]]
+
+        for i in range(rowIndex):
+            temp = [0] + res[-1] + [0]
+            row = []
+            for j in range(len(res[-1]) + 1):
+                row.append(temp[j]+temp[j+1])
+            res.append(row)
+        print(res[-1])
+        return res[-1]
+
+# getRow(5)
+# omit row -1 to become it's index     couting original res as first line
