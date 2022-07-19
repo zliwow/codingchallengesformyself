b6d42909966adeb6ad852ec687ad9aadc91c10f1
Andrew Zihao Li <zliwow123@gmail.com>
2022-07-18 21:43:54 -0700

    modified:   coding practices.py

--- coding practices.py
+++ coding practices.py
@@ -100,3 +100,17 @@ def evalPRN(tokens):
     return stack[0]
     
 evalPRN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
+
+# 112. Path Sum 
+def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+    def dfs(node, curSum):
+        if not node:
+            return False
+        
+        curSum += node.val
+        if not node.left and not node.right:
+            return curSum == targetSum
+        
+        return (dfs(node.left, curSum) or 
+                dfs(node.right, curSum))
+    return dfs(root, 0)
