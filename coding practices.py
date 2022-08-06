59436a16f827a38b5e36261159e9fa5691020a63
Andrew Zihao Li <zliwow123@gmail.com>
2022-08-05 20:52:17 -0700

    modified:   coding practices.py

--- coding practices.py
+++ coding practices.py
@@ -751,3 +751,31 @@ def findMin(nums):
             r = mid - 1
     print(res)
 findMin([2,1])
+
+# 206. Reverse Linked List
+def reverseList(head):
+    curr = head
+    prev = None
+    while curr: # while curr != None
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+    return prev
+
+# 21. Merge Two Sorted Lists Linked
+def mergeTwoLists(l1,l2):
+    dum = ListNode()
+    tail = dum.next
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next= l2
+            l2 = l2.next
+    if l1:
+        tail.next = l1
+    elif l2:
+        tail.next = l2
+    return dum.next
