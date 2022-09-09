09ff2a98728797e0f8ffe0fba71c6e8df8b50245
Andrew Zihao Li <zliwow123@gmail.com>
2022-09-09 14:03:29 -0700

    modified:   coding practices.py

--- coding practices.py
+++ coding practices.py
@@ -1326,3 +1326,21 @@ def hasCycle(head):
             return True
         
     return False
+
+#142. Linked List Cycle II  tortoise and hare
+def detectCycle(head):
+    if not head: return None # nocycle
+    
+    slow, fast = head, head
+    while fast.next and fast.next.next:
+        slow = slow.next
+        fast = fast.next.next
+        if fast == slow: break
+    if not fast.next or not fast.next.next: return None # nocycle
+    
+    slow2 = head
+    
+    while slow.next:
+        if slow == slow2: return slow
+        slow = slow.next
+        slow2 = slow2.next
