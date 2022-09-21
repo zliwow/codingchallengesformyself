9e0c6944b2688e641fff0db44a151f1506b6a7be
Andrew Zihao Li <zliwow123@gmail.com>
2022-09-21 15:39:08 -0700

    modified:   coding practices.py

--- coding practices.py
+++ coding practices.py
@@ -1692,3 +1692,34 @@ def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
     return (self.isSameTree(p.left, q.left) and
         self.isSameTree(p.right, q.right))
     
+# 450. Delete Node in a BST
+# O(h) height of tree
+# traverse the tree finding matching key. Then in case 4, get the minimal val of last left key and replace the matching key with it. Delete the last left node afterward.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) :
+        if not root:
+            return
+        if root.val == key:
+            # 4 cases 
+            if not root.left and not root.right: return None
+            if not root.left and root.right: return root.right
+            if root.left and not root.right: return root.left
+            # if both 
+            pnt = root.right
+            while pnt.left: pnt = pnt.left
+            root.val = pnt.val
+            root.right = self.deleteNode(root.right, root.val)
+            
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            root.right = self.deleteNode(root.right, key)
+            
+        return root
+                
+                
